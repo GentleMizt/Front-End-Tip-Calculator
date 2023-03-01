3df41ef244d22b41a1c6ab8558d0645cead393cd
GentleMizt <olamideiyanda18@gmail.com>
2023-03-01 22:28:36 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -2,3 +2,20 @@
     const ppleInp = document.querySelector('.ppleInp');
     const tipPerPerson = document.getElementById('tip-amnt');
     const totalPerPerson = document.getElementById('total-amnt');
+
+
+    billInp.addEventListener('input', billInpFunc);
+    ppleInp.addEventListener('input', ppleInpFunc);
+
+    let billValue = 0;
+    let ppleValue = 1;
+
+    function billInpFunc(){
+        billValue = parseFloat(billInp.value);
+        console.log(billValue);
+    }
+
+    function ppleInpFunc(){
+        ppleValue = parseFloat(ppleInp.value);
+        console.log(ppleValue);
+    }
