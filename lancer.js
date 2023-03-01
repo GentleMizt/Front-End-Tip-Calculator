05d79452193065040a931845e4842eff911dd6cc
GentleMizt <olamideiyanda18@gmail.com>
2023-03-01 22:21:33 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -1,2 +1,4 @@
     const billInp = document.querySelector('.billInp');
     const ppleInp = document.querySelector('.ppleInp');
+    const tipPerPerson = document.getElementById('tip-amnt');
+    const totalPerPerson = document.getElementById('total-amnt');
