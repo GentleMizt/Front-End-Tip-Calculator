4246753c255aa0684b8dd96560751dbe7938d02a
GentleMizt <olamideiyanda18@gmail.com>
2023-03-01 22:39:36 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -2,10 +2,13 @@
     const ppleInp = document.querySelector('.ppleInp');
     const tipPerPerson = document.getElementById('tip-amnt');
     const totalPerPerson = document.getElementById('total-amnt');
-
+    const tips = document.querySelectorAll('.gr-1');
 
     billInp.addEventListener('input', billInpFunc);
     ppleInp.addEventListener('input', ppleInpFunc);
+    
+
+
 
     billInp.value = '0.0';
     ppleInp.value = '1';
